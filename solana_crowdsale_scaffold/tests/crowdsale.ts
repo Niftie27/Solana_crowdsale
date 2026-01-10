84d1aaef6845e70a7664cad07f190887dbf93157
Tomas Pazout <tom.pazout@gmail.com>
2026-01-10 17:25:08 +0100

    create Helpers file for tests

--- solana_crowdsale_scaffold/tests/crowdsale.ts
+++ solana_crowdsale_scaffold/tests/crowdsale.ts
@@ -1,16 +1,19 @@
 import * as anchor from "@coral-xyz/anchor"
-import { PublicKey } from '@solana/web3.js'
+import { PublicKey, VOTE_PROGRAM_ID } from '@solana/web3.js'
 import { getAssociatedTokenAddressSync } from '@solana/spl-token'
 import { expect } from 'chai'
 import { Crowdsale } from "../target/types/crowdsale"
 
 describe("Crowdsale", () => {
   // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env()
-  const connection = provider.connection
+  // Setting up our test environment (normally we use metamask for that)
+  const provider = anchor.AnchorProvider.env() // method from Anchor to create local environment for testing
+  const connection = provider.connection // connection coming from provider
   anchor.setProvider(provider)
 
   // Our overall program
+  // let us call upon all methods that we coded in lib.rs
+  // load idl (interface) for crowdsale program
   const program = anchor.workspace.Crowdsale as anchor.Program<Crowdsale>
 
   // Our main account. This is who will create 
@@ -28,4 +31,38 @@ describe("Crowdsale", () => {
   console.log(`Creator Public Key: ${creator.publicKey}`)
   console.log(`Crowdsale Public Key: ${crowdsaleKeypair.publicKey}`)
   console.log(`Buyer Public Key: ${buyerKeypair.publicKey}\n`)
+
+  const ID = crowdsaleKeypair.publicKey
+  const COST = 1
+
+  /* --- SETUP CROWDSALE AUTHORITY --- */
+  // In order to transfer tokens, we need to create the authority for the crowdsale. 
+  // This will be based of the ID of the crowdsale.
+  // This account stores data for crowdsale.
+  // Can sign token transfers on behalf of crowdsale
+  // We pull crowdsalePDA (the state of the crowdsale)
+  // There also has to be account that signs off on tranfers for crowdsale
+  const crowdsalePDA = PublicKey.findProgramAddressSync(
+    [ID.toBuffer()],
+    anchor.workspace.Crowdsale.programId
+  )[0]
+
+  const crowdsaleAuthorityPDA = PublicKey.findProgramAddressSync(
+    [ID.toBuffer()], 'authority',
+    anchor.workspace.Crowdsale.programId
+  )[0]
+
+    
+  console.log(`Crowdsale Key: ${crowdsalePDA}`)
+
+  console.log(`Crowdsale Authority: ${crowdsaleAuthorityPDA}`)
+
+  // crowdsaleTokenAccount holds tokens for buyers
+
+  let mintKeypair, crowdsaleTokenAccount
+
+  before(async () => {
+    
+  })
+
 });
